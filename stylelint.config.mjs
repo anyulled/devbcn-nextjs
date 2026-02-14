e755560a5bcb578731effb9e406d34b5bb5769ea
Anyul Rivas <anyulled@gmail.com>
2026-02-14 01:22:00 +0100

    chore: setup Stylelint, fix build error, and configure git pre-commit hooks

---
+++ stylelint.config.mjs
@@ -0,0 +1,21 @@
+const config = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss"
+  ],
+  rules: {
+    "no-descending-specificity": null,
+    "selector-class-pattern": null,
+    "alpha-value-notation": "number",
+    "color-function-notation": null,
+    "scss/at-rule-no-unknown": true,
+    "scss/comment-no-empty": null,
+    "custom-property-pattern": null,
+    "keyframes-name-pattern": null,
+    "declaration-block-no-shorthand-property-overrides": null,
+    "block-no-empty": null,
+    "number-max-precision": null
+  }
+};
+
+export default config;
