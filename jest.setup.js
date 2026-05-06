2535995e554dfed3ad0a156ea3ec7cc6b652a894
Anyul Rivas <anyulled@gmail.com>
2026-05-06 19:02:15 +0200

    fix: remove @next/third-parties/google dependency to unblock CI

--- jest.setup.js
+++ jest.setup.js
@@ -1,14 +1,6 @@
 import "@testing-library/jest-dom";
 import "whatwg-fetch";
 
-// Mock analytics libraries
-jest.mock("@next/third-parties/google", () => ({
-  sendGTMEvent: jest.fn(),
-  sendGAEvent: jest.fn(),
-  GoogleTagManager: () => null,
-  GoogleAnalytics: () => null,
-}));
-
 jest.mock("@vercel/analytics", () => ({
   track: jest.fn(),
 }));
